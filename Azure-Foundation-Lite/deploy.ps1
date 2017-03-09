f77974a2b9f5d73189a7d2ecce6b539dd2af3a1e
OddbjornHoyland <oddbjorn.hoyland@ucom.no>
2017-03-09 14:40:59 +0100

    Feil

--- Azure-Foundation-Lite/deploy.ps1
+++ Azure-Foundation-Lite/deploy.ps1
@@ -1,9 +1,9 @@
 ﻿Param(
-        [string] $ResourceGroupName = "Ucom",
+        [string] $ResourceGroupName = "Feil",
         [ValidateSet("West Europe","North Europe","East US","East US 2","Central US","North Central US","South Central US","West Central US","West US","West US 2")] 
         [string] 
         $ResourceGroupLocation = "West Europe",
-        [string] $MGMTResourceGroupName = "Ucom"
+        [string] $MGMTResourceGroupName = "Feil"
 
     )
 
@@ -229,7 +229,7 @@ $OptionalParametersMGMT.Add($rdsVmResourceGroupName, $ResourceGroupName)
 
 # Set appVmName parameter
 $appVMValue = $azureDeployParams.parameters.appVmName.value
-Set-Variable appVmName 'appVmName' -Option ReadOnly -Force
+Set-Variable appVmName 'sqlVmName' -Option ReadOnly -Force
 $OptionalParametersMGMT.Add($appVmName, $appVMValue)
 
 # Set appVmResourceGroupName parameter
